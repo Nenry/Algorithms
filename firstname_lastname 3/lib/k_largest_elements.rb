4a5aad88205426c641be4f592055620bdadadf75
HN <hhnguyen92@gmail.com>
2018-07-05 13:09:33 -0700

    Complete k_largest_element

--- firstname_lastname 3/lib/k_largest_elements.rb
+++ firstname_lastname 3/lib/k_largest_elements.rb
@@ -1,5 +1,14 @@
 require_relative 'heap'
+# require_relative 'heap_sort'
 
 def k_largest_elements(array, k)
+  heaped = BinaryMinHeap.new
 
+  array.each do |num|
+    heaped.push(num)
+    heaped.extract if heaped.count == k + 1
+  end
+  # until results.length == k
+  # end 
+  return heaped.store
 end
