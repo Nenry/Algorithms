edd2c3ce741e6c7857059c2b2f11a8c47d55765f
HN <hhnguyen92@gmail.com>
2018-07-05 11:36:32 -0700

    Complete heap_sort

--- firstname_lastname 3/lib/heap_sort.rb
+++ firstname_lastname 3/lib/heap_sort.rb
@@ -2,6 +2,16 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    
+    # p self
+    self.each_index do |i|
+      BinaryMinHeap.heapify_up(self, i) {|el1, el2| el2 <=> el1}
+    end
+    # p self
+
+    (self.length - 1).downto(1) do |i|
+      self[0], self[i] = self[i], self[0]
+      BinaryMinHeap.heapify_down(self, 0, i) {|el1, el2| el2 <=> el1}
+    end 
+  #  self
   end
 end
