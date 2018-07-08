592b07299c06f7c88da7a0c54a7dad39ed113af0
HN <hhnguyen92@gmail.com>
2018-07-08 11:24:13 -0700

    Implement insert algorithm

--- BST/lib/binary_search_tree.rb
+++ BST/lib/binary_search_tree.rb
@@ -1,11 +1,19 @@
 # There are many ways to implement these methods, feel free to add arguments 
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_reader :root
   def initialize
+    @root = nil 
   end
 
   def insert(value)
+    if !root
+      @root = BSTNode.new(value)
+    else 
+      traverse(value)
+    end 
   end
 
   def find(value, tree_node = @root)
@@ -30,5 +38,25 @@ class BinarySearchTree
 
   private
   # optional helper methods go here:
+  def traverse(value)
+    start = @root
+    found = false
+    until found
+      if value <= start.value && !start.left 
+        start.left = BSTNode.new(value)
+        found = true
+      elsif value > start.value && !start.right 
+        start.right = BSTNode.new(value)
+        found = true
+      elsif value <= start.value && start.left.value 
+        start = start.left
+        found = false 
+      elsif value > start.value && start.right.value
+        start = start.right
+        found = false 
+      end
+    end 
+  end 
+
 
 end
--- BST/lib/bst_node.rb
+++ BST/lib/bst_node.rb
@@ -9,4 +9,6 @@ class BSTNode
     @parent = nil 
   end
 
+  
+
 end
