af75aa29df678ed8b248245b36056bac6afc1912
HN <hhnguyen92@gmail.com>
2018-07-08 14:39:02 -0700

    Devise delete and replacement function for nodes

--- BST/lib/binary_search_tree.rb
+++ BST/lib/binary_search_tree.rb
@@ -15,8 +15,10 @@ class BinarySearchTree
       parent_node = traverse(value)
       if value <= parent_node.value
         parent_node.left = BSTNode.new(value)
+        parent_node.left.parent = parent_node
       elsif value > parent_node.value 
         parent_node.right = BSTNode.new(value)
+        parent_node.right.parent = parent_node
       end 
       
     end 
@@ -39,10 +41,63 @@ class BinarySearchTree
   end
 
   def delete(value)
+    delete_node = find(value)
+    # p delete_node.value
+    replacement_node = children?(delete_node) > 0 ? maximum(delete_node) : nil
+      # p replacement_node.value
+      # p replacement_node.value
+    if @root.value == value
+      @root = nil
+      return  
+    end 
+
+    if delete_node.parent.left == delete_node  
+      # p delete_node.parent.left
+      delete_node.parent.left = replacement_node  
+    elsif delete_node.parent.right == delete_node
+      delete_node.parent.right = replacement_node
+    end
+
+    if replacement_node
+     if replacement_node.parent.left == replacement_node
+        replacement_node.parent.left = replacement_node.left
+     end 
+     if replacement_node.parent.right == replacement_node
+        replacement_node.parent.right = replacement_node.left
+     end 
+    end
+
+
+    # if !replacement_node.nil?
+    #   replacement_node.parent.left = replacement_node.left
+    #   replacement_node.parent.right = replacement_node.right
+    # end 
+      
+
+
+
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    maximum_node = tree_node
+    left = tree_node.left 
+    right = tree_node.right 
+    if left && right
+      maximum_node = maximum_node.left.right
+      until !maximum_node.right
+        maximum_node = maximum_node.right
+      end 
+      return maximum_node
+    elsif left && !right
+      maximum_node = left
+      return maximum_node
+    elsif !left && right
+      maximum_node = maximum_node.right  
+      return maximum_node
+    elsif children?(tree_node)
+      return nil 
+    end 
   end
 
   def depth(tree_node = @root)
@@ -82,6 +137,21 @@ class BinarySearchTree
       # end
     end 
     curr_node
+  end
+  
+  def children?(node)
+    child_count = 0 
+
+    if node.left 
+      child_count += 1
+    end 
+
+    if node.right 
+      child_count += 1
+    end 
+
+    return child_count
+    
   end 
 
 
