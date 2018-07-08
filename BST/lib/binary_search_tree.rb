b2e575f921c71fb0cbef82e3fb91c8a83cf7c145
HN <hhnguyen92@gmail.com>
2018-07-08 11:40:14 -0700

    Implement node finding function

--- BST/lib/binary_search_tree.rb
+++ BST/lib/binary_search_tree.rb
@@ -12,11 +12,30 @@ class BinarySearchTree
     if !root
       @root = BSTNode.new(value)
     else 
-      traverse(value)
+      parent_node = traverse(value)
+      if value <= parent_node.value
+        parent_node.left = BSTNode.new(value)
+      elsif value > parent_node.value 
+        parent_node.right = BSTNode.new(value)
+      end 
+      
     end 
   end
 
   def find(value, tree_node = @root)
+    curr_node = tree_node
+
+    until !curr_node
+      if value == curr_node.value 
+        return curr_node
+      elsif value <= curr_node.value 
+        curr_node = curr_node.left
+      elsif value > curr_node.value  
+        curr_node = curr_node.right 
+      end 
+    end 
+    curr_node
+
   end
 
   def delete(value)
@@ -39,23 +58,30 @@ class BinarySearchTree
   private
   # optional helper methods go here:
   def traverse(value)
-    start = @root
-    found = false
-    until found
-      if value <= start.value && !start.left 
-        start.left = BSTNode.new(value)
-        found = true
-      elsif value > start.value && !start.right 
-        start.right = BSTNode.new(value)
-        found = true
-      elsif value <= start.value && start.left.value 
-        start = start.left
-        found = false 
-      elsif value > start.value && start.right.value
-        start = start.right
-        found = false 
-      end
+    curr_node = @root
+    
+    until !curr_node.left && value <= curr_node.value ||  !curr_node.right && value > curr_node.value
+      
+      if value <= curr_node.value
+        curr_node = curr_node.left
+      elsif value > curr_node.value
+        curr_node = curr_node.right
+      end 
+      # if value <= start.value && !start.left 
+      #   start.left = BSTNode.new(value)
+      #   found = true
+      # elsif value > start.value && !start.right 
+      #   start.right = BSTNode.new(value)
+      #   found = true
+      # elsif value <= start.value && start.left.value 
+      #   start = start.left
+      #   found = false 
+      # elsif value > start.value && start.right.value
+      #   start = start.right
+      #   found = false 
+      # end
     end 
+    curr_node
   end 
 
 
