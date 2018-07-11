4aca183bf3eb1474d62c23b0fef5373686cc8232
HN <hhnguyen92@gmail.com>
2018-07-11 13:39:02 -0700

    Complete frog hops bottom up

--- Dynamic_Programming/lib/dynamic_programming.rb
+++ Dynamic_Programming/lib/dynamic_programming.rb
@@ -1,20 +1,61 @@
+require 'byebug'
+
 class DynamicProgramming
 
   def initialize
-
+    @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = {}
   end
 
   def blair_nums(n)
-
-  end
-
-  def frog_hops_bottom_up(n)
+    return @blair_cache[n] if @blair_cache[n]
+            
+    n_one_less_odd = (n - 1) * 2 - 1
+    @blair_cache[n] = blair_nums(n - 1) + blair_nums(n - 2)  + n_one_less_odd
 
   end
 
   def frog_cache_builder(n)
+    possible_hops = [1, 2, 3]
+    cache = {0 => [], 1 => [[1]], 2 => [[1, 1], [2]], 3 => [[1, 1, 1], [1, 2], [2, 1], [3]]}
+    # return nil if n < 0
+    return cache if n < 4
+    (4..n).each do |n|
+      # debugger  
+      cache[n] = cache[n - 1].map {|el| el + [1]} + cache[n - 2].map{|el| el + [2]} + cache[n - 3].map {|el| el + [3]}
+    end 
+
+    cache
+    
 
   end
+
+  def frog_hops_bottom_up(n)
+    cache = frog_cache_builder(n)
+
+    cache[n]
+    
+  end
+
+
+
+#   def fib_cache_builder(n)
+#   # Builds the cache, starting at 1 and ending at n
+#   cache = { 1 => 1, 2 => 1 }
+#   return cache if n < 3
+#   (3..n).each do |i|
+#     cache[i] = cache[i - 1] + cache[i - 2]
+#   end
+
+#   cache
+# end
+
+# def fibonacci(n)
+#   # Calls the helper function
+#   cache = fib_cache_builder(n)
+#   # Returns the nth entry
+#   cache[n]
+# end
 
   def frog_hops_top_down(n)
 
